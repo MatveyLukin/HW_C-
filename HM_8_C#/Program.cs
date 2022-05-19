b1aebe5df8a82a6b794ff04da03130ecfff9f293
unknown <luk.matvey@yandex.ru>
2022-05-19 18:01:59 +0300

    2 task is done,almost

--- HM_8_C#/Program.cs
+++ HM_8_C#/Program.cs
@@ -74,24 +74,67 @@
 
 // // Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
 
-// int[,] SumRowMatrix(int[,] matr)
-// {
+int[,] CreateRandomMatrix(int row, int column)
+{
+    int[,] matrix = new int[row,column];
 
-//     // result;
-//     for (int i = 0; i < matr.GetLength(0); i++)
-//     {
-//         int sum = 0;
-//         for (int j = 0; j < matr.GetLength(1); j++)
-//         {
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            matrix[i,j] = new Random().Next(1,10);
 
-//             sum += matr[i, j];
-//             j++;
-//             Console.Write($" {sum} ");
-//         }
+    return matrix;
+}
 
-//     }
-//     return matr;
-// }
+void PrintMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+            Console.Write($"{matr[i,j]} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();    
+}
+void MinSumString(int[,] matr)
+{
+    int sumString = 0,
+        minValue = 0,
+        minIndex = 0;
+    for (int j = 0; j < matr.GetLength(1); j++)
+            minValue += matr[0,j];
+            
+    for (int i = 1; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++){
+            sumString += matr[i,j];
+        }
+        Console.WriteLine(sumString);
+            
+        if(sumString < minValue) 
+        {
+            minValue = sumString;
+            minIndex = i;
+        }
+        
+        sumString = 0;  
+    
+    }
+    Console.WriteLine("Строка с наименьшей суммой элементов имеет номер => " + (minIndex+1));
+    Console.WriteLine();
+}
+int q = 3;
+int l = 3; // dimension of rectangular array
+if(q != l){
+    Console.WriteLine("Введен неравномерный массив");
+}
+else
+{
+int[,] matrix2 = CreateRandomMatrix(row: q, column: l);
+PrintMatrix(matrix2);
+MinSumString(matrix2);
+}
+
+
 
 // // Задача №3
 // // Условие: Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
@@ -113,61 +156,67 @@
 // // 11 16 15 6
 // // 10 9 8 7
 
-int[,] CreateRandomMatrix(int row, int column)
-{
-    int[,] matr = new int[row, column];
-    for (int i = 0; i < matr.GetLength(0); i++)
-    {
-        for (int j = 0; j < matr.GetLength(1); j++)
-        {
-            matr[i, j] = new Random().Next(1, 10);
-            if (row == column)
-                Console.Write($"{matr[i, j]} ");
-            else
-                Console.WriteLine("Массив не является равносторонним"); // не забыть убрать для последуюющих задач
-        }
-        Console.WriteLine();
-    }
-    return matr;
-}
-void PrintMatrix(int[,] matr)
-{
-    for (int i = 0; i < matr.GetLength(0); i++)
-    {
-        for (int j = 0; j < matr.GetLength(1); j++)
 
-            Console.WriteLine();
-    }
-}
-Console.WriteLine();
-int[,] matr = CreateRandomMatrix(3, 3);
-Console.WriteLine();
-PrintMatrix(SumRowMatrix(matr));
-int[,] SumRowMatrix(int[,] matr)
-{
-int[] SumLane = (sum);
-int MinSum = SumLane[0];
-for (int i = 1; i < SumLane.length; i++)
-{
-  if (SumLane[i] < MinSum)
-  {
-      MinSum = SumLane[i];
-  }  
-}
-Console.WriteLine($"{SumLane[i] + 1}");
-    // result;
-    for (int i = 0; i < matr.GetLength(0); i++)
-    {
-        int sum = 0;
-        for (int j = 0; j < matr.GetLength(1); j++)
-        {
 
-            sum += matr[i, j];
-            
-            
-        }
-        Console.Write($" {sum} ");
 
-    }
-    return matr;
-}
+
+
+
+
+//////////////////////////////////////////////////////////////////
+
+// int [,] CreateMatrix (int row, int col)
+// {
+//     int [,] array = new int [row, col];
+//     for (int i = 0; i < row; i++)
+//     {
+//         for (int j =0; j < col; j++)
+//         {
+//             array [i, j] = new Random().Next (10, 99);
+//         }
+//     }
+//     return array;
+// }
+
+// void PrintMatrix(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write($"{array[i, j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// int MinSumRow (int [,] matrix)
+// {
+//     int minSum=0;
+//     int minIndex=0;
+//     for (int i = 0; i<matrix.GetLength(0); i++)
+//     {
+//         int sum = 0;
+//         for (int j = 0; j<matrix.GetLength(1); j++)
+//         {
+//             sum = sum + matrix[i,j];
+//         }
+//         Console.WriteLine (sum);
+//         if (minSum>sum || i==0)
+//         {
+//             minSum=sum;
+//             minIndex=i;
+//         }
+//     }
+//     Console.WriteLine ();
+//     return minIndex;
+// }
+// int row = 3;
+// int col = 4;
+// int [,] matrix = CreateMatrix (row,col);
+// if(q != l){
+//     Console.WriteLine("Введен неравномерный массив");
+// }
+// PrintMatrix (matrix);
+// Console.WriteLine ();
+// Console.WriteLine (MinSumRow (matrix));
